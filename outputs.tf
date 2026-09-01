5978df51edbcdec144e9fa6f4aa8f09869e8191e
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2026-08-31 21:02:28 -0500

    prueba

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_id" {
-    value = aws_s3_bucket_app.app_bucket.id
+    value = aws_s3_bucket.app_bucket.id
 }
 
 output "bucket_arn" {
-    value = aws_s3_bucket_app.app_bucket.arn
+    value = aws_s3_bucket.app_bucket.arn
 }
